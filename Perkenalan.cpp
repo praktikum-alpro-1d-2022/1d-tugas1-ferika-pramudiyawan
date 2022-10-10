d09a43d9f20323a04cec350ccf9e50c5ecee809c
Ferika pramudiyawan <114640064+Ferr27@users.noreply.github.com>
2022-10-10 20:20:44 +0700

    Add files via upload
    uplod file

---
+++ Perkenalan.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int umur = 18;
+   string npm = "2210631170122";
+   string nama = "Ferika Pramudiyawan";
+   string kelas = "1D Informatika";
+   string hobi = "Bermain futsal";
+   string cita = "Menjadi Pengusaha sukses";
+   cout << "NPM: " << npm << endl;
+   cout << "Nama Lengkap: " << nama << endl;
+   cout << "Umur: " << umur << endl;
+   cout << "Kelas: " << kelas << endl;
+   cout << "Hobi: " << hobi << endl;
+   cout << "Cita-cita: " << cita << endl;
+
+   return 0;
+}
